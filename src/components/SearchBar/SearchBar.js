0408bc4518ca186d6a43910447d2e225ac01a095
Jessica-Renard <75540937+Jessica-Renard@users.noreply.github.com>
2021-09-06 10:28:42 -0400

    Update SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -79,7 +79,7 @@ class SearchBar extends React.Component {
           <input placeholder="Where?" onChange={this.handleLocationChange} />
         </div>
         <div className="SearchBar-submit">
-           <a onClick={this.handleSearch} {/* eslint-disable-line react/jsx-no-target-blank */} >
+           <a onClick={this.handleSearch}> {/* eslint-disable-line react/jsx-no-target-blank */} 
              Let's Go</a>
         </div>
       </div>
